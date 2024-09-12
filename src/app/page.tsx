89dab2f89e7d0e014f1c5a5ab903475be38f7313
gjsoy24 <gour.joy24@gmail.com>
2024-09-12 21:09:35 +0600

    working on navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import ArrowRight from '@/assets/arrow-right.svg';
+import Logo from '@/assets/logosaas.png';
+import MenuIcon from '@/assets/menu.svg';
+import Image from 'next/image';
+
 export default function Home() {
-	return <h1> Hello World </h1>;
+	return (
+		<header className='sticky top-0 z-50'>
+			<div className='flex justify-center items-center py-3 bg-black text-white'>
+				<div className='inline-flex items-center gap-1'>
+					<p>Get started by for free</p>
+					<ArrowRight className='w-4 h-4' />
+				</div>
+			</div>
+			<div className='py-5'>
+				<div className='container'>
+					<div className='flex items-center justify-between'>
+						<Image src={Logo} height={40} width={40} alt='Sass Logo' />
+						<MenuIcon className='w-6 h-6' />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
